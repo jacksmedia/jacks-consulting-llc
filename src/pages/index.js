5a5228b94f78e256d92f0376106bfd4400f8a5ec
jacksmedia <alex@j4cks.com>
2021-12-18 16:48:16 -0800

    :bug: legible landing page!
    whew

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ const BlogIndex = ({ data, location }) => {
               {({isVisible})=>(
                 <Post key={post.fields.slug} isVisible={isVisible} className="svg-hexes-snowfall">
                   <article
-                    className="post-list-item svg-hexes center"
+                    className="post-list-item svg-hexes"
                     itemScope
                     itemType="http://schema.org/Article"
                   >
